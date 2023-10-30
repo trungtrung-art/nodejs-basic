23819f27af7a7e088f3ec5791c4d1c7eac90a11a
Trung Tran Thien <trung.tranthien@elinext.com>
2023-10-30 10:44:25 +0700

    trungtran - update upload single file with multer

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,39 +1,71 @@
 import connection from '../configs/connectDB'
+import multer from 'multer';
 
 export const getHomepage = async (req, res) => {
 
-  const [rows,fields]= await connection.execute('SELECT * FROM users')
+  const [rows, fields] = await connection.execute('SELECT * FROM users')
   return res.render('index.ejs', { dataUser: rows })
-  
+
 }
 
 export const getDetailUser = async (req, res) => {
-  const {userId} = req.params;
-  const [user,fields]= await connection.execute(`SELECT * FROM users WHERE id = ${userId}`)
+  const { userId } = req.params;
+  const [user, fields] = await connection.execute(`SELECT * FROM users WHERE id = ${userId}`)
   return res.send(JSON.stringify(user[0]))
 }
 
-export const createNewUser = async (req,res) => {
-  const {firstName,lastName,email,address} = req.body;
+export const createNewUser = async (req, res) => {
+  const { firstName, lastName, email, address } = req.body;
   await connection.execute(`INSERT INTO users (firstName, lastName, email, address) VALUES ('${firstName}','${lastName}','${email}','${address}')`)
   return res.redirect('/')
 }
 
-export const deleteUser = async (req,res)  => {
-  const {userId} = req.params;
+export const deleteUser = async (req, res) => {
+  const { userId } = req.params;
   await connection.execute(`DELETE FROM users WHERE id = ${userId}`)
   return res.redirect('/')
 }
 
-export const getEditUser = async (req,res) => {
-  const {userId} = req.params;
-  const [user,fields]= await connection.execute(`SELECT * FROM users WHERE id = ${userId}`)
-  return res.render('update.ejs',{ dataUser: user[0] })
+export const getEditUser = async (req, res) => {
+  const { userId } = req.params;
+  const [user, fields] = await connection.execute(`SELECT * FROM users WHERE id = ${userId}`)
+  return res.render('update.ejs', { dataUser: user[0] })
 }
 
-export const putUpdateUser= async (req,res) => {
-  const {firstName,lastName,email,address} = req.body;
-  const {userId} = req.params;
+export const putUpdateUser = async (req, res) => {
+  const { firstName, lastName, email, address } = req.body;
+  const { userId } = req.params;
   await connection.execute(`UPDATE users SET firstName='${firstName}',lastName='${lastName}',email='${email}',address='${address}' WHERE id=${userId}`)
   return res.redirect('/')
+}
+
+export const getUploadFilesPage = async (req, res) => {
+  return res.render('uploadFiles.ejs')
+}
+
+
+const upload = multer().single('profile_pic');
+
+export const handleUploadFile = async (req, res) => {
+  // 'profile_pic' is the name of our file input field in the HTML form
+  upload(req, res, function (err) {
+    // req.file contains information of uploaded file
+    // req.body contains information of text fields, if there were any
+
+    if (req.fileValidationError) {
+      return res.send(req.fileValidationError);
+    }
+    else if (!req.file) {
+      return res.send('Please select an image to upload');
+    }
+    else if (err instanceof multer.MulterError) {
+      return res.send(err);
+    }
+    else if (err) {
+      return res.send(err);
+    }
+
+    // Display uploaded image for user validation
+    res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
+  });
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,27 +1,56 @@
 import express from "express";
-import { getHomepage,getDetailUser,createNewUser,deleteUser,getEditUser,putUpdateUser } from "../controller/homeController";
+import { getHomepage, getDetailUser, createNewUser, deleteUser, getEditUser, putUpdateUser, getUploadFilesPage, handleUploadFile } from "../controller/homeController";
+import multer from "multer";
+import path from "path";
 
+var appRoot = require('app-root-path')
 let router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, appRoot + '/src/public/image/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+
+// helper function
+const imageFilter = function (req, file, cb) {
+  // Accept images only
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    req.fileValidationError = 'Only image files are allowed!';
+    return cb(new Error('Only image files are allowed!'), false);
+  }
+  cb(null, true);
+};
+
+let upload = multer({ storage: storage, fileFilter: imageFilter });
 
 const initWebRouter = (app) => {
   //setup router with design patten MVC without model
   router.get('/', getHomepage);
+  router.get('/home', getHomepage);
 
   router.get('/detail/user/:userId', getDetailUser);
 
   router.post('/create-new-user', createNewUser);
 
-  router.post('/delete/user/:userId',deleteUser)
+  router.post('/delete/user/:userId', deleteUser)
 
-  router.get('/edit-user/:userId',getEditUser)
+  router.get('/edit-user/:userId', getEditUser)
 
-  router.post('/update-user/:userId',putUpdateUser)
-  
+  router.post('/update-user/:userId', putUpdateUser)
+
   router.get('/about', (req, res) => {
     res.send('HI IM TRUNGTRUNGCUM')
   })
 
-  return app.use('/',router)
+  router.get('/upload', getUploadFilesPage);
+  router.post('/upload-profile-pic', upload.single('profile_pic'), handleUploadFile)
+
+  return app.use('/', router)
 }
 
 export default initWebRouter;
