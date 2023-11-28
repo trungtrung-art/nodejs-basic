348ff162cca82b38ff5a01f8fe665c9cbfd84330
Trung Tran Thien <trung.tranthien@elinext.com>
2023-11-28 12:02:00 +0700

    learn middleware with morgan

--- src/server.js
+++ src/server.js
@@ -3,12 +3,21 @@ import configViewEngine from './configs/configViewengine'
 import initWebRouter from './routes/web'
 import initApiRouter from './routes/api'
 import connection from './configs/connectDB'
+
 import 'dotenv/config'
+const morgan = require('morgan')
 
 const app = express()
 const port = process.env.PORT || 8080; // backup with port 8080 if you forget import dotenv
 
-app.use(express.urlencoded({extended: true}));
+app.use((req, res, next) => {
+  console.log('>> run into my middleware')
+  console.log(req.method)
+  next()
+})
+
+app.use(morgan('combined'))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //setup view engine
@@ -19,6 +28,11 @@ initWebRouter(app)
 
 //init api router
 initApiRouter(app)
+
+// handle 404 not found
+app.use((req, res) => {
+  return res.render('404.ejs')
+})
 
 
 app.listen(port, () => {
